bfafe9563c5875cd6606f1c48d7ff38d8fb2751d
Usuario <Usuario@10.22.148.84>
2019-09-25 14:21:35 +0100

    first project

--- src/kata1/Kata1.java
+++ src/kata1/Kata1.java
@@ -1,14 +1,16 @@
 
 package kata1;
-
+import java.time.LocalDate;
+import java.time.Month;
 import java.util.Date;
 
 
 public class Kata1 {
 
     public static void main(String[] args) {
-        Person person = new Person("Andrea",new Date());
+    Person person= new Person("person",new Date(80,1,10));
         System.out.println(person);
-    }
     
-}
+    
+    }
+    }
