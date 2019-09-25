3bb1e7df7e08fec69b04ff030eebef51e1acd453
Usuario <Usuario@10.22.148.84>
2019-09-25 13:54:33 +0100

    first project

--- src/kata1/Kata1.java
+++ src/kata1/Kata1.java
@@ -1,11 +1,14 @@
 
 package kata1;
 
+import java.util.Date;
+
 
 public class Kata1 {
 
     public static void main(String[] args) {
-        
+        Person person = new Person("Andrea",new Date());
+        System.out.println(person);
     }
     
 }
